88cfb550dc027a832505056ac2969efc69466f86
Andreas Kemnade <andreas@kemnade.info>
2017-02-26 21:32:02 +0100

    add simple-vibra which was always silently expected

--- Makefile
+++ Makefile
@@ -1,18 +1,20 @@
 PREFIX=/usr/local
 
-all: simple-gsmgui gsmgui-ring.sh gsmgui-start-audio.sh gsmgui-hungup.sh start-gsmgui.sh
+all: simple-gsmgui gsmgui-ring.sh gsmgui-start-audio.sh gsmgui-hungup.sh start-gsmgui.sh simple_vibra
 simple-gsmgui: gsmgui.vala gsmmodem.vala
 	valac $^ -o simple-gsmgui --pkg gtk+-2.0 --pkg posix
+
+simple_vibra: simple_vibra.c
 
 %.sh: %.in
 	sed "s-PREFIX-$(PREFIX)-g" <$^ >$@
 
 install: 
-	install -m 755 -t $(DESTDIR)$(PREFIX)/bin simple-gsmgui gsmgui-ring.sh gsmgui-start-audio.sh gsmgui-hungup.sh start-gsmgui.sh
+	install -m 755 -t $(DESTDIR)$(PREFIX)/bin simple-gsmgui gsmgui-ring.sh gsmgui-start-audio.sh gsmgui-hungup.sh start-gsmgui.sh simple_vibra
 	install -m 755 -d $(DESTDIR)$(PREFIX)/share/gsmgui
 	install -m 644 -t $(DESTDIR)$(PREFIX)/share/gsmgui voice-4 voice
 
 clean:
-	rm -f simple-gsmgui
+	rm -f simple-gsmgui simple_vibra gsmgui-start-audio.sh
 
 .PHONY: install clean all
---
+++ simple_vibra.c
@@ -0,0 +1,54 @@
+#include <linux/input.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv) 
+{
+  int i,id,j;
+  int starttime;
+  int fd;
+  struct input_event *ev;
+  if (argc < 3) {
+    printf("Usage: %s vibrafile ontime offtime ..",argv[0]);
+  }
+  fd=open(argv[1],O_RDWR);
+  if (fd<0) {
+    fprintf(stderr,"cannot open %s\n",argv[1]);
+    return 1;
+  }
+  argc-=2;
+  argv++;
+  argv++; 
+  ev=malloc(sizeof(struct input_event)*argc);
+  j=0;
+  starttime=0;
+  for(i = 0; i < argc; i++) {
+    int t=atoi(argv[i]);
+    if ((i&1)==0) {
+      struct ff_effect ff;
+      ff.type=FF_RUMBLE;
+      ff.id=-1;
+      ff.direction=0;
+      ff.trigger.button=0;
+      ff.trigger.interval=0;
+      ff.u.rumble.strong_magnitude=0xffff;
+      ff.u.rumble.weak_magnitude=0; 
+      ff.replay.length=t;
+      ff.replay.delay=starttime;
+      if (0<=ioctl(fd,EVIOCSFF,&ff)) {
+        ev[j].time.tv_sec=0;
+        ev[j].time.tv_usec=0;
+        ev[j].type=0x15;
+        ev[j].code=ff.id;
+        ev[j].value=1;
+        j++;
+      }
+    }
+    starttime+=t;
+  } 
+  write(fd,ev,j*sizeof(struct input_event));
+  usleep(starttime*1000);
+  close(fd);
+}
